2150e60d0c4050f16473f786f3e05c1dc8520ada
Madison <maddiewebb188@yahoo.com>
2023-09-25 14:14:13 -0500

    backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new pg.Client('postgres://localhost/petclub_db');
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.json());
 
 const homePage = path.join(__dirname, 'index.html');
 app.get('/', (req, res)=> res.sendFile(homePage));
@@ -15,6 +16,47 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
+
+app.get('/api/pets', async (req, res, next) => {
+  try {
+    const SQL = `
+    SELECT *
+    FROM pets
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows)
+  } catch (error) {
+    next (error);
+  }
+});
+
+app.get('/api/owners', async (req, res, next) => {
+  try {
+    const SQL = `
+      SELECT *
+      FROM owners
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows)
+  } catch (error) {
+    next (error);
+  }
+});
+
+app.put('/api/pets/:id', async (req, res, next) => {
+  try {
+    const SQL = `
+      UPDATE pets
+      SET owner_id = $1, name = $2
+      WHERE id = $3
+      RETURNING *
+    `;
+    const response = await client.query(SQL, [req.body.owner_id, req.body.name, req.params.id]);
+    res.send(response.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+})
 
 const init = async()=> {
   await client.connect();
